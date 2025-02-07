deb46bd48a11b35028e045e6ca7e77453f0cfeb8
zaferyildirim-1 <zaferyildirim1283@gmail.com>
2025-02-07 09:47:20 +0000

    main py dosyasinda yorum satiri kisaltildi

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 adres = "accounts.csv"
 
-# :pushpin: Dosya açma fonksiyonu (varsa okuma, yoksa oluşturma)
+# :Dosya açma
 def DosyaAc(adres):
     if not os.path.exists(adres):
         with open(adres, "w", encoding="UTF-8") as dosya:
